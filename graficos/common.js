daa42331343df846b30a80e1c9a642ce9ca21e6d
guilhermelima <guilhermelimaruby@gmail.com>
2024-02-19 17:02:22 -0300

    unidade_10

--- graficos/common.js
+++ graficos/common.js
@@ -3,4 +3,10 @@ const getCSS = (variavel) => {
     return bodyStyles.getPropertyValue(variavel)
 }
 
-export {getCSS}
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -20,8 +20,34 @@ async function quantidadeUsuariosPorRede() {
 
     const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
-
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
     }
 
     const grafico = document.createElement('div')
