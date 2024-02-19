2bb4be7dff42737791674efd5ee7628ee19b9746
guilhermelima <guilhermelimaruby@gmail.com>
2024-02-19 10:14:17 -0300

    unidade_5

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,9 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais()
